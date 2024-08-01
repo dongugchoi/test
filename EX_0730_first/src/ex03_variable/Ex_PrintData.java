4813d776c0dd391646dba2a55f4ef46b3ff0b6d8
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-08-01 09:00:57 +0900

    Add files via upload

---
+++ EX_0730_first/src/ex03_variable/Ex_PrintData.java
@@ -0,0 +1,25 @@
+package ex03_variable;
+
+public class Ex_PrintData {
+	public static void main(String[] args) {
+		// 정수형 데이터 : 소수점이 없는 숫자 1, 24, 100, -50, -189 ...
+		// 실수형 데이터 : 소수점이 있는 숫자 1.23, 3.14 ...
+		// 문자형 데이터 : 홑따옴표로 감싼 한글자 'a', 'f', 'e', 'u'...
+		// 문자열 데이터 : 쌍따옴표로 감싼 문장 "Hello World", "Hello" ...
+		
+		System.out.println(100);
+		System.out.println(100+50);
+		// 문자열앞뒤에 오는 더하기는 "연결의 의미를 가지고 있다."
+		// 안녕하세요50
+		// 진행방향은 왼쪽에서 오른쪽이다.
+		System.out.println("안녕하세요"+50);
+		
+		System.out.println(5+10+":"+(5+10));
+		
+		
+		System.out.println("2 + 2 = " + (2 + 2));
+		
+		
+		
+	}
+}
